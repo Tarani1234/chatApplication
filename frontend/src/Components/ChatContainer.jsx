74dae5e42060a331babfafc64bccd7162a65b577
Tarani1234 <tmaliyala@techdeets.net>
2025-07-29 16:04:17 +0530

    sidebar ui has been updated

--- frontend/src/Components/ChatContainer.jsx
+++ frontend/src/Components/ChatContainer.jsx
@@ -1,11 +1,65 @@
-import React from 'react'
+import React from "react";
+import assets, { messagesDummyData } from "../assets/assets";
 
-const ChatContainer = () => {
-  return (
-    <div>
-       <h1>Chat Container</h1>
+const ChatContainer = ({ selectedUser, setSelectedUser }) => {
+  return selectedUser ? (
+    <div className="h-full overflow-scroll relative backdrop-blur-lg ">
+      {/* header part */}
+      <div className="flex items-center gap-3 py-3 mx-4 border-b border-stone-500">
+        <img src={assets.profile_martin} className="w-8 rounded-full" alt="" />
+        <p className="flex-1 text-lg text-white flex items-center gap-2">
+          Martin Johnson
+          <span className="w-2 h-2 rounded-full bg-green-500"></span>
+        </p>
+        <img
+          onClick={() => setSelectedUser(null)}
+          src={assets.arrow_icon}
+          alt=""
+          className="md:hidden max-w-7"
+        />
+        <img src={assets.help_icon} alt="" className="max-md:hidden max-w-5" />
+      </div>
+      {/* chart area  */}
+      <div className="flex flex-col h-[calc(100%-120px)] overflow-y-scroll p-3 pb-6">
+        {messagesDummyData.map((msg, i) => (
+          <div
+          key={i}
+          className={`flex items-end gap-2 justify-end mb-4
+            ${msg.senderId !== "680f5116f10f3cd28382ed02" && 'flex-row-reverse'
+              }`}
+        >
+            {msg.image ? (
+              <img src={msg.image} alt="" />
+            ) : (
+              <p
+                className={`p-2 max-w-[200px] md:text-sm font-light rounded-lg mb-8 break-all
+                bg-violet-500/30 text-white ${
+                  msg.senderId === "680f5116f10f3cd28382ed02"
+                    ? "rounded-br-none"
+                    : "rounded-bl-none"
+                }`}
+              >
+                {msg.text}
+              </p>
+            )}
+
+              <div className="text-center text-xs ">
+                   <img src={msg.senderId === '680f5116f10f3cd28382ed02' ?
+                    assets.avatar_icon : assets.profile_martin} alt="" className="w-8 rounded-full "/>
+                    <p className="text-gray-500">{msg.createdAt}</p>
+                </div>
+          </div>
+        ))}
+      </div>
+
+      
     </div>
-  )
-}
+  ) : (
+    <div className="flex flex-col items-center justify-center h-full text-gray-500 bg-white/10 mx-md:hidden">
+      <img src={assets.logo_icon} alt="" className="max-w-16" />
+      <p className="text-lg font-medium text-white">Chat anytime, anywhere</p>
+    </div>
+  );
+};
 
-export default ChatContainer
+export default ChatContainer;
